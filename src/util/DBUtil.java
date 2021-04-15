764be3db619a768dea6b5afdf2e53963db277c10
Hoosss <hsnam1211@naver.com>
2021-04-15 22:31:34 +0900

    DB connect 방법을 기존 하나하나 연결하고 끊던 것을JNDI Datasource을 사용해 방법을 변경

--- src/util/DBUtil.java
+++ src/util/DBUtil.java
@@ -2,25 +2,49 @@ package util;
 
 import java.sql.*;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
 public class DBUtil {
 	
 	// Connecting
+//	public static Connection getConnection() {
+//		
+//		Connection conn = null;
+//		String driverName = "oracle.jdbc.OracleDriver";
+//		String userid = "LIBRARY", password = "1234";
+//		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+//		
+//		try {
+//			Class.forName(driverName);
+//			conn = DriverManager.getConnection(url, userid, password);
+//			//System.out.println("Driver Connect Success");
+//		} catch (ClassNotFoundException | SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		
+//		
+//		return conn;
+//	}
+	
 	public static Connection getConnection() {
-		
 		Connection conn = null;
-		String driverName = "oracle.jdbc.OracleDriver";
-		String userid = "LIBRARY", password = "1234";
-		String url = "jdbc:oracle:thin:@localhost:1521:xe";
-		
+		Context initContext;
 		try {
-			Class.forName(driverName);
-			conn = DriverManager.getConnection(url, userid, password);
-			//System.out.println("Driver Connect Success");
-		} catch (ClassNotFoundException | SQLException e) {
+			initContext = new InitialContext();
+			Context envContext  = (Context)initContext.lookup("java:/comp/env");
+			DataSource ds = (DataSource)envContext.lookup("jdbc/myoracle");
+			conn = ds.getConnection();
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 		
 		return conn;
 	}
